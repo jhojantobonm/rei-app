7c69eab9827a5e151053cad4c21466aa134de1b5
Jhojan Tobón <jhojantobonm@gmail.com>
2025-02-17 16:47:29 -0500

    📦 Install and configure vitest
    ✅ Add test for Home page

---
+++ src/tests/Home.test.tsx
@@ -0,0 +1,18 @@
+import { beforeAll, describe, expect, it } from "vitest";
+import { Home } from "../pages/Home";
+import { render, screen } from "@testing-library/react";
+
+describe('Test Home page',()=>{
+  let home: HTMLElement;
+  
+  beforeAll(()=>{
+    render(<Home/>);
+    screen.debug();
+  
+    home = screen.getByTestId('home-container');
+  });
+  
+  it('should render Home page',()=>{
+    expect(home).toBeInTheDocument();
+  });
+})
--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,9 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss(),],
-
+  test: {
+    environment: 'happy-dom',
+    setupFiles: ['./src/tests/setup.ts'],
+    include: ['src/**/*.{test,spec}.{ts,tsx}']
+  },
 })
