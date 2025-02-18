d3f21f94fbc3157716e566c9c403f7cd98139e58
Jhojan Tobón <jhojantobonm@gmail.com>
2025-02-18 13:10:46 -0500

    ✨ ♻️Enhance some styles and refactor Home.tsx

--- src/components/MenuInformationItem.tsx
+++ src/components/MenuInformationItem.tsx
@@ -2,14 +2,11 @@ import './MenuInformationItem.css'
 
 export const MenuInformationItem = ({ src }:{src?: string}) => {
   return (
-    <>
       <li data-testid='menu-info-item'>
-        <button className='cursor-pointer'>
+        <button className='cursor-pointer w-[5rem]'>
           <img id="menu-info-item-image" 
           src={src} alt={`Image of ${src}`}/>
         </button>
       </li>
-    
-    </>
   )
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import { ContentInformation } from '../components/ContentInformation'
+import { ContentInformationSolar } from '../components/ContentInformationSolar'
 import { MenuInformation } from '../components/MenuInformation'
 import './Home.css'
 
@@ -13,8 +14,14 @@ export const Home = () => {
       <h1 className="text-center text-3xl font-bold mt-5">INFORMATION</h1> 
     </header>
     <section className='pl-6 pr-6'>
-      <MenuInformation className='mt-10' />
-      <ContentInformation/>
+      <MenuInformation className='mt-3' />
+      <ContentInformation className='mt-7 text-[1.4rem]' 
+      title='Solar Energy' 
+      image='https://images.pexels.com/photos/356036/pexels-photo-356036.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'>
+        <ContentInformationSolar/>
+        
+      </ContentInformation>
+    
 
     </section>
   </section>
