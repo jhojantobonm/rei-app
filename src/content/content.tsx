75387436457bc7a1067014410de6d2e4ad5313bc
Jhojan Tobón <jhojantobonm@gmail.com>
2025-02-19 11:28:21 -0500

    📄✨ Create content.tsx to separate the content along the content components

---
+++ src/content/content.tsx
@@ -0,0 +1,33 @@
+import { ContentInformationBioFuel } from "../components/ContentInformationBioFuel";
+import { ContentInformationGeothermal } from "../components/ContentInformationGeothermal";
+import { ContentInformationHydro } from "../components/ContentInformationHydro";
+import { ContentInformationSolar } from "../components/ContentInformationSolar";
+import { ContentInformationWind } from "../components/ContentInformationWind";
+
+export const content = [
+  {
+    title:'Hydro Energy',
+    image:'https://images.pexels.com/photos/1633943/pexels-photo-1633943.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    description: <ContentInformationHydro/> 
+  },
+  {
+    title:'Wind Energy',
+    image:'https://images.pexels.com/photos/414837/pexels-photo-414837.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    description: <ContentInformationWind/> 
+  },
+  {
+    title:'Solar Energy',
+    image:'https://images.pexels.com/photos/356036/pexels-photo-356036.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    description: <ContentInformationSolar/> 
+  },
+  {
+    title:'Bio Fuel Energy',
+    image:'https://images.pexels.com/photos/3129643/pexels-photo-3129643.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    description: <ContentInformationBioFuel/> 
+  },
+  {
+    title:'Geothermal Energy',
+    image:'https://images.pexels.com/photos/7403905/pexels-photo-7403905.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    description: <ContentInformationGeothermal/> 
+  },
+];
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,23 @@
 import { ContentInformation } from '../components/ContentInformation'
-import { ContentInformationSolar } from '../components/ContentInformationSolar'
 import { FooterApp } from '../components/FooterApp'
 import { HeaderApp } from '../components/HeaderApp'
 import { MenuInformation } from '../components/MenuInformation'
+import { content } from '../content/content'
 import './Home.css'
 
 
 export const Home = () => {
+
   return (
   <section data-testid='home-container'>
     <HeaderApp title='INFORMATION'/>
     <section className='pl-6 pr-6 md:pl-[9rem] md:pr-[9rem]'>
       <MenuInformation className='mt-3 md:mt-12' />
-      <ContentInformation className='mt-7 text-[1.4rem]' 
-      title='Solar Energy' 
-      image='https://images.pexels.com/photos/356036/pexels-photo-356036.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'>
-        <ContentInformationSolar/>
-        
-      </ContentInformation>
+      {
+        content.map(item=><ContentInformation className='mt-7 text-[1.4rem]' 
+          title={item.title} 
+          image={item.image}>{item.description}</ContentInformation>)
+      }
     
 
     </section>
