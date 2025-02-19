f8568a36ab83a722b8d87fb9636eca13464d43d1
Jhojan Tobón <jhojantobonm@gmail.com>
2025-02-19 16:05:16 -0500

    📦 Install framer-motion and add scale motion

--- src/components/ContentInformation.tsx
+++ src/components/ContentInformation.tsx
@@ -1,5 +1,7 @@
-import { ReactNode } from "react"
+import { ReactNode, useEffect, useState } from "react"
+import { motion } from "framer-motion";
 import './ContentInformationSolar.css'
+import { useContextApp } from "../context/useContextApp";
 
 interface ContentInfoProps extends React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
   title?: string,
@@ -8,19 +10,37 @@ interface ContentInfoProps extends React.DetailedHTMLProps<React.HTMLAttributes<
 }
 
 export const ContentInformation = ({title,image,children,...props}: ContentInfoProps) => {
+  const {contentId} = useContextApp();
+  const [isScaled, setIsScaled] = useState(false);
+  useEffect(()=>{
+    setIsScaled(true);
+    const timeId =  setTimeout(() => {
+      setIsScaled(false);
+    }, 500);
+    return ()=>{
+      clearTimeout(timeId);
+    }
+  },[contentId])
+
 
   return (
       <div data-testid='content-info' {...props} className=' mt-7 text-[1.4rem]'>
-        <h2 className="text-3xl font-semibold">{title}</h2>
-        <div className="md:flex md:flex-row-reverse">
-          <figure className="md:w-[50%] md:h-[25rem] mt-3.5 rounded-2xl overflow-hidden shadow-2xl">
-            <img src={image} alt={`This is an image about ${title}`}
-            className="w-full h-full object-fill" />
-          </figure>
-          <div className="md:w-[50%] mr-6">
-            {children}
+        <motion.div
+            animate={{ scale: isScaled ? 0 : 1 }}
+            transition={{type: 'spring', duration: 0.5,bounce:0.5 }}
+        >
+
+          <h2 className="text-3xl font-semibold">{title}</h2>
+          <div className="md:flex md:flex-row-reverse">
+            <figure className="md:w-[50%] md:h-[25rem] mt-3.5 rounded-2xl overflow-hidden shadow-2xl">
+              <img src={image} alt={`This is an image about ${title}`}
+              className="w-full h-full object-fill" />
+            </figure>
+            <div className="md:w-[50%] mr-6">
+              {children}
+            </div>
           </div>
-        </div>
+        </motion.div>
       </div>
   )
 }
