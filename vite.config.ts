f33b3665ff8b4c75ee683f8fe69da1ccd60bcb23
Jhojan Tobón <jhojantobonm@gmail.com>
2025-02-19 09:29:05 -0500

    🐛 🔧 Fix vite configuration by creating 2 separated configuration for both vite and vitest and merge them

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,19 @@
-import { defineConfig } from 'vite'
+import { defineConfig as defineConfigVite,mergeConfig} from 'vite'
+import { defineConfig as defineConfigVitest} from 'vitest/config'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
+const viteConfig =  defineConfigVite({
   plugins: [react(),tailwindcss(),],
+});
+
+const vitestConfig =  defineConfigVitest({
   test: {
     environment: 'happy-dom',
     setupFiles: ['./src/tests/setup.ts'],
     include: ['src/**/*.{test,spec}.{ts,tsx}']
   },
-})
+});
+
+export default mergeConfig(viteConfig,vitestConfig);
