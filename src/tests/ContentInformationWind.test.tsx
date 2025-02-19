d29eb74d86564a2be8b16dfdfd872421d18f7daa
Jhojan Tobón <jhojantobonm@gmail.com>
2025-02-19 11:22:46 -0500

    ✅ Create ContentInformationWind.test.tsx

---
+++ src/tests/ContentInformationWind.test.tsx
@@ -0,0 +1,18 @@
+import { beforeAll, describe, expect, it } from "vitest";
+import { render, screen } from "@testing-library/react";
+import { ContentInformationWind } from "../components/ContentInformationWind";
+
+describe('Test ContentInformationWind component',()=>{
+  let wind: HTMLElement;
+  
+  beforeAll(()=>{
+    render(<ContentInformationWind/>);
+    screen.debug();
+  
+    wind = screen.getByTestId('content-info-wind');
+  });
+  
+  it('should render ContentInformationWind component',()=>{
+    expect(wind).toBeInTheDocument();
+  });
+})
