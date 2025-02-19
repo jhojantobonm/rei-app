aa18ecf6904ae339298f00d55d6e18688a785484
Jhojan Tobón <jhojantobonm@gmail.com>
2025-02-19 11:25:43 -0500

    ✅ Create ContentInformationBioFuel.test.tsx

---
+++ src/tests/ContentInformationBioFuel.test.tsx
@@ -0,0 +1,18 @@
+import { beforeAll, describe, expect, it } from "vitest";
+import { render, screen } from "@testing-library/react";
+import { ContentInformationBioFuel } from "../components/ContentInformationBioFuel";
+
+describe('Test ContentInformationBioFuel component',()=>{
+  let bioFuel: HTMLElement;
+  
+  beforeAll(()=>{
+    render(<ContentInformationBioFuel/>);
+    screen.debug();
+  
+    bioFuel = screen.getByTestId('content-info-biofuel');
+  });
+  
+  it('should render ContentInformationBioFuel component',()=>{
+    expect(bioFuel).toBeInTheDocument();
+  });
+})
