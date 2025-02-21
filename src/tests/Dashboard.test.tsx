967c62c4fa9acf5b9f6faa183d6883e5f9d5574e
Jhojan Tobón <jhojantobonm@gmail.com>
2025-02-21 13:41:36 -0500

    ✅ Create Dashboard.test.tsx

---
+++ src/tests/Dashboard.test.tsx
@@ -0,0 +1,18 @@
+import { beforeAll, describe, expect, it } from "vitest";
+import { Dashboard } from "../pages/Dashboard";
+import { render, screen } from "@testing-library/react";
+
+describe('Test Dashboard page',()=>{
+  let calculator: HTMLElement;
+  
+  beforeAll(()=>{
+    render(<Dashboard/>);
+    screen.debug();
+  
+    calculator = screen.getByTestId('dashboard-container');
+  });
+  
+  it('should render Dashboard page',()=>{
+    expect(calculator).toBeInTheDocument();
+  });
+})
