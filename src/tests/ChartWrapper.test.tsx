a26a2229faeb92480e3b5dbf25ae9d01a74b52e2
Jhojan Tobón <jhojantobonm@gmail.com>
2025-02-21 15:58:32 -0500

    ✅ Create ChartWrapper.test.tsx

---
+++ src/tests/ChartWrapper.test.tsx
@@ -0,0 +1,18 @@
+import { beforeAll, describe, expect, it } from "vitest";
+import { ChartWrapper } from "../components/ChartWrapper";
+import { render, screen } from "@testing-library/react";
+
+describe('Test ChartWrapper component',()=>{
+  let barsChart: HTMLElement;
+  
+  beforeAll(()=>{
+    render(<ChartWrapper/>);
+    screen.debug();
+  
+    barsChart = screen.getByTestId('chart-wrapper-component');
+  });
+  
+  it('should render ChartWrapper component',()=>{
+    expect(barsChart).toBeInTheDocument();
+  });
+})
