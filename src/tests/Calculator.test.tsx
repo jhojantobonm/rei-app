2150fc930ca686253b7da52d93ffb6519da32b70
Jhojan Tobón <jhojantobonm@gmail.com>
2025-02-21 10:08:35 -0500

    ✅ Create Calculator.test.tsx

---
+++ src/tests/Calculator.test.tsx
@@ -0,0 +1,18 @@
+import { beforeAll, describe, expect, it } from "vitest";
+import { Calculator } from "../pages/Calculator";
+import { render, screen } from "@testing-library/react";
+
+describe('Test Calculator page',()=>{
+  let calculator: HTMLElement;
+  
+  beforeAll(()=>{
+    render(<Calculator/>);
+    screen.debug();
+  
+    calculator = screen.getByTestId('calculator-container');
+  });
+  
+  it('should render Calculator page',()=>{
+    expect(calculator).toBeInTheDocument();
+  });
+})
