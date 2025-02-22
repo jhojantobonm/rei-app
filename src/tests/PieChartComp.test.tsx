f81c45700c58ef0c61b2a6eaa1fe68b9cc15c179
Jhojan Tobón <jhojantobonm@gmail.com>
2025-02-22 15:36:48 -0500

    ✅ Create PieChartComp.test.tsx

---
+++ src/tests/PieChartComp.test.tsx
@@ -0,0 +1,18 @@
+import { beforeAll, describe, expect, it } from "vitest";
+import { PieChartComp } from "../components/PieChartComp";
+import { render, screen } from "@testing-library/react";
+
+describe('Test PieChartComp component',()=>{
+  let barsChart: HTMLElement;
+  
+  beforeAll(()=>{
+    render(<PieChartComp/>);
+    screen.debug();
+  
+    barsChart = screen.getByTestId('pie-chart-component');
+  });
+  
+  it('should render PieChartComp',()=>{
+    expect(barsChart).toBeInTheDocument();
+  });
+})
