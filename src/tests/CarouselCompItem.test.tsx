a56bbacf061cda4fd7606a97718f0e3da737df09
Jhojan Tobón <jhojantobonm@gmail.com>
2025-02-22 14:48:43 -0500

    ✅ Create CarouselCompItem.tsx

---
+++ src/tests/CarouselCompItem.test.tsx
@@ -0,0 +1,18 @@
+import { beforeAll, describe, expect, it } from "vitest";
+import { CarouselCompItem } from "../components/CarouselCompItem";
+import { render, screen } from "@testing-library/react";
+
+describe('Test CarouselCompItem component',()=>{
+  let carouselItem: HTMLElement;
+  
+  beforeAll(()=>{
+    render(<CarouselCompItem/>);
+    screen.debug();
+  
+    carouselItem = screen.getByTestId('carousel-item-component');
+  });
+  
+  it('should render CarouselCompItem component',()=>{
+    expect(carouselItem).toBeInTheDocument();
+  });
+})
