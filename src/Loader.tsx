a2533157bf5b95344be7b55be1c6ef38c9c1bc5d
Jhojan Tobón <jhojantobonm@gmail.com>
2025-02-23 13:14:01 -0500

    📄 Create and add Loader.tsx

---
+++ src/Loader.tsx
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
+import App from "./App";
+
+export function Loader(){
+  const [isLoading, setIsLoading] = useState(true);
+  
+  useEffect(() => {
+    const timer = setTimeout(() => setIsLoading(false), 500); // Simulate loading
+    return () => clearTimeout(timer); // Cleanup timeout
+  }, []);
+  return<>
+  {isLoading ? (<div className="w-screen h-screen flex justify-center items-center animate-spin">
+    <AiOutlineLoading3Quarters size={'3rem'}/>
+
+  </div>) : <App /> }
+  </> 
+}
+
+
+
+
+
+
--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
 import { AppProvider } from './context/ProviderApp.tsx'
 import { BrowserRouter } from 'react-router-dom'
+import { Loader } from './Loader.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <AppProvider>
-        <App />
+        <Loader />
       </AppProvider>
     </BrowserRouter>
   </StrictMode>,
