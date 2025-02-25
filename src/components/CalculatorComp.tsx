70a13aa89b92e5b99080fcb21c280969a01b5a5e
Jhojan Tobón <jhojantobonm@gmail.com>
2025-02-25 16:02:48 -0500

    ✨Add year selector to the calculator

--- src/components/CalculatorComp.tsx
+++ src/components/CalculatorComp.tsx
@@ -1,12 +1,43 @@
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { Button } from "./ui/button"
+import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from "./ui/select"
+
+const years: number[] = []
+for (let i = 1965; i <= 2022; i++) {
+  years.push(i)
+}
+
 
 
 export const CalculatorComp = ()=>{
 
+
+  const handleMousedown = (size: number)=>{
+    
+  }
+
+  
   return(
     <div data-testid='calculator-component' className="flex flex-col gap-7 md:gap-20">
+      <div>
+        <Select>
+          <SelectTrigger className="w-[180px] bg-white text-[1.4rem] p-8 rounded-2xl">
+            <SelectValue placeholder="Select the year"/>
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              <SelectLabel className="text-[1.4rem]">Year</SelectLabel>
+                {years.map((size) => (
+                  <SelectItem key={size} value={size + ''} onMouseDown={()=>handleMousedown(size)} className="text-[1.4rem]">
+                    {size}
+                  </SelectItem>
+                ))}
+              </SelectGroup>
+          </SelectContent>
+
+        </Select>
+      </div>
       <div>
         <Label htmlFor="consumption" className="text-[1.4rem] font-light">* Total electrical consumption (kWh):</Label>
         <Input type="number" id="consumption" placeholder="Example: 18" className="bg-white mt-2 p-7 text-[1.4rem] md:text-[1.4rem] placeholder:text-gray-300" />
--- src/components/DataSelector.tsx
+++ src/components/DataSelector.tsx
@@ -9,29 +9,6 @@ import {
 } from "@/components/ui/select"
 import { useContextApp } from "@/context/useContextApp"
 
-
-// const files = [
-//   "01-renewable-share-energy.csv",
-//   "02-modern-renewable-energy-consumption.csv",
-//   "03-modern-renewable-prod.csv",
-//   "04-share-electricity-renewables.csv",
-//   "05-hydropower-consumption.csv",
-//   "06-hydro-share-energy.csv",
-//   "07-share-electricity-hydro.csv",
-//   "08-wind-generation.csv",
-//   "09-cumulative-installed-wind-energy-capacity-gigawatts.csv",
-//   "10-wind-share-energy.csv",
-//   "11-share-electricity-wind.csv",
-//   "12-solar-energy-consumption.csv",
-//   "13-installed-solar-PV-capacity.csv",
-//   "14-solar-share-energy.csv",
-//   "15-share-electricity-solar.csv",
-//   "16-biofuel-production.csv",
-//   "17-installed-geothermal-capacity.csv",
-//   "18-conventional-energy-consumption.csv",
-// ]
-
-
 export const DataSelector = () => {
   const {fileList,setSelectedFile} = useContextApp();
  
