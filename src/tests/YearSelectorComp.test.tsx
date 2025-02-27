779d4228bcc3163710321f53163365d06e042291
Jhojan Tobón <jhojantobonm@gmail.com>
2025-02-27 11:51:22 -0500

    ✅ Create YearSelectorComp.test.tsx

---
+++ src/tests/YearSelectorComp.test.tsx
@@ -0,0 +1,18 @@
+import { beforeAll, describe, expect, it } from "vitest";
+import { YearSelector } from "../components/YearSelector";
+import { render, screen } from "@testing-library/react";
+
+describe('Test YearSelector component',()=>{
+  let yearSelector: HTMLElement;
+  
+  beforeAll(()=>{
+    render(<YearSelector/>);
+    screen.debug();
+  
+    yearSelector = screen.getByTestId('year-selector-component');
+  });
+  
+  it('should render YearSelector component',()=>{
+    expect(yearSelector).toBeInTheDocument();
+  });
+})
