720405d1903644db7853155f3510711bddb75b57
Jhojan Tobón <jhojantobonm@gmail.com>
2025-02-28 13:07:34 -0500

    🐛 Chance to cors policy

--- src/utils/dataFetch.ts
+++ src/utils/dataFetch.ts
@@ -2,7 +2,7 @@ import { config } from "@/config/config";
 
 export const fetchList = async ()=>{
   const apiUrl = import.meta.env.VITE_API_URL
-  const response = await fetch(`${apiUrl}/data/list`,{cache: 'force-cache', mode: 'no-cors', headers: {
+  const response = await fetch(`${apiUrl}/data/list`,{cache: 'force-cache', mode: 'cors', headers: {
     "Content-Type": "application/json"
   }});
   const data = await response.json();
@@ -10,7 +10,7 @@ export const fetchList = async ()=>{
 }
 
 export const fetchFile = async (file:string)=>{
-  const response = await fetch(`${config.apiUrl}/data/${file}`,{cache: 'force-cache', mode: 'no-cors', headers: {
+  const response = await fetch(`${config.apiUrl}/data/${file}`,{cache: 'force-cache', mode: 'cors', headers: {
     "Content-Type": "application/json"
   }});
   const data = await response.json();
@@ -18,7 +18,7 @@ export const fetchFile = async (file:string)=>{
 }
 
 export const fetchRenewableData = async (year?:string)=>{
-  const response = await fetch(`${config.apiUrl}/calculator?year=${year}`,{cache: 'force-cache', mode: 'no-cors', headers: {
+  const response = await fetch(`${config.apiUrl}/calculator?year=${year}`,{cache: 'force-cache', mode: 'cors', headers: {
     "Content-Type":"application/json"
   }});
   const data = await response.json();
@@ -26,7 +26,7 @@ export const fetchRenewableData = async (year?:string)=>{
 }
 
 export const fetchBarsChartData = async (year?:string)=>{
-  const response = await fetch(`${config.apiUrl}/bars-chart?year=${year}`,{cache: 'force-cache', mode: 'no-cors', headers: {
+  const response = await fetch(`${config.apiUrl}/bars-chart?year=${year}`,{cache: 'force-cache', mode: 'cors', headers: {
     "Content-Type":"application/json"
   }});
   const data = await response.json();
@@ -34,7 +34,7 @@ export const fetchBarsChartData = async (year?:string)=>{
 }
 
 export const fetchPieChartData = async (year?:string)=>{
-  const response = await fetch(`${config.apiUrl}/pie-chart?year=${year}`,{cache: 'force-cache', mode: 'no-cors', headers: {
+  const response = await fetch(`${config.apiUrl}/pie-chart?year=${year}`,{cache: 'force-cache', mode: 'cors', headers: {
     "Content-Type":"application/json"
   }});
   const data = await response.json();
@@ -42,7 +42,7 @@ export const fetchPieChartData = async (year?:string)=>{
 }
 
 export const fetchLineChartData = async ()=>{
-  const response = await fetch(`${config.apiUrl}/line-chart`,{cache: 'force-cache', mode: 'no-cors', headers: {
+  const response = await fetch(`${config.apiUrl}/line-chart`,{cache: 'force-cache', mode: 'cors', headers: {
     "Content-Type":"application/json"
   }});
   const data = await response.json();
@@ -50,7 +50,7 @@ export const fetchLineChartData = async ()=>{
 }
 
 export const fetchAreaChartData = async ()=>{
-  const response = await fetch(`${config.apiUrl}/area-chart`,{cache: 'force-cache', mode: 'no-cors', headers: {
+  const response = await fetch(`${config.apiUrl}/area-chart`,{cache: 'force-cache', mode: 'cors', headers: {
     "Content-Type":"application/json"
   }});
   const data = await response.json();
