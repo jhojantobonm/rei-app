3f459b7820619edfa345fc8b787c218553fcb739
Jhojan Tobón <jhojantobonm@gmail.com>
2025-03-04 11:14:12 -0500

    ✨ Add german language

--- src/ i18n.ts
+++ src/ i18n.ts
@@ -2,6 +2,7 @@ import i18n from "i18next";
 import { useTranslation, initReactI18next } from "react-i18next";
 import enJSON from "@/locales/en.json"
 import esJSON from "@/locales/es.json"
+import deJSON from "@/locales/de.json"
 
 
 i18n
@@ -10,6 +11,7 @@ i18n
       resources: {
          en: {...enJSON},
          es: {...esJSON},
+         de: {...deJSON},
       }, // Where we're gonna put translations' files
       lng: "en",     // Set the initial language of the App
 
